089b7844cd2ab4b617c0a961fe9ab9f582ff57cb
Oumar Yanni <oumaryanni@gmail.com>
2024-01-19 17:38:24 +0000

    Adding npm i

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 function Page() {
+  const firstName = "Thomas";
+
   return (
     <main className="flex items-center justify-center min-h-screen w-screen">
       <h1 data-testid="title">Hello, World!</h1>
     </main>
-  )
+  );
 }
 
-export default Page
-
+export default Page;
