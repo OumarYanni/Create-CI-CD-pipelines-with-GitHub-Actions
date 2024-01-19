2bf0472322e2639a545cc038df20c066a50b7c53
Oumar Yanni <oumaryanni@gmail.com>
2024-01-19 17:46:26 +0000

    Insert test error

--- src/utils/sum/index.spec.ts
+++ src/utils/sum/index.spec.ts
@@ -1,14 +1,13 @@
-import {describe, expect, it} from "vitest";
+import { describe, expect, it } from "vitest";
 
 import makeSum from ".";
 
-describe('makeSum Unit Test Suites', () => {
-  it('should return something', () => {
-    expect(makeSum(2, 2)).toBeDefined()
-  })
+describe("makeSum Unit Test Suites", () => {
+  it("should return something", () => {
+    expect(makeSum(2, 2)).toBeDefined();
+  });
 
-  it('should return 4', () => {
-    expect(makeSum(2, 2)).toEqual(4)
-  })
-})
-
+  it("should return 4", () => {
+    expect(makeSum(2, 2)).toEqual(5);
+  });
+});
